9d7dd5274b3a1f6112fb99f5149b1fa73bb11bd6
Fitrah Arie Ramadhan <56785130+Fitrah1812@users.noreply.github.com>
2021-06-18 21:12:24 +0700

    Update client.c

--- client/client.c
+++ client/client.c
@@ -1,4 +1,3 @@
-
 #include <stdio.h>
 #include <string.h>
 #include <stdlib.h>
@@ -9,17 +8,11 @@
 #include <errno.h>
 #include <netdb.h>
 #include <netinet/in.h>
-
- 
-#define DATA_BUFFER 200
-
-//global variable
+#define DATA_BUFFER 300
 const int SIZE_BUFFER = sizeof(char) * DATA_BUFFER;
 char username[DATA_BUFFER] = {0};
 char type[DATA_BUFFER] = {0};
 bool wait = false;
- 
-// setup function
 void *handleInput(void *client_sockets);
 void *checkOutput(void *client_sockets);
 void serverOutput(int fd, char *input);
@@ -58,6 +51,42 @@ int createSocket(){
     return fd;
 }
 
+//void storeToLogs(char *username, char *command)
+//{
+//    time(&my_time);
+//	timeinfo = localtime(&my_time);
+//
+//	char day[10], month[10], year[10], hour[10], minute[10], second[10];
+//
+//	sprintf(day, "%d", timeinfo->tm_mday);
+//	if(timeinfo->tm_mday < 10)
+//		sprintf(day, "0%d", timeinfo->tm_mday);
+//
+//	sprintf(month, "%d", timeinfo->tm_mon+1);
+//	if(timeinfo->tm_mon+1 < 10)
+//		sprintf(month, "0%d", timeinfo->tm_mon+1);
+//
+//	sprintf(year, "%d", timeinfo->tm_year+1900);
+//
+//	sprintf(hour, "%d", timeinfo->tm_hour);
+//	if(timeinfo->tm_hour < 10)
+//		sprintf(hour, "0%d", timeinfo->tm_hour);
+//
+//	sprintf(minute, "%d", timeinfo->tm_min);
+//	if(timeinfo->tm_min < 10)
+//		sprintf(minute, "0%d", timeinfo->tm_min);
+//
+//	sprintf(second, "%d", timeinfo->tm_sec);
+//	if(timeinfo->tm_sec < 10)
+//		sprintf(second, "0%d", timeinfo->tm_sec);
+//
+//    char content[arraySize];
+//    sprintf(content, "%s-%s-%s %s:%s:%s:%s:%s", year, month, day, hour, minute, second, username, command);
+//
+//    FILE* fptr_logs = fopen(logPath, "a");
+//    fprintf(fptr_logs, "%s\r\n", content);
+//    fclose(fptr_logs);
+//}
 
 
 bool loginStatus(int fd, int argc, char *argv[]){
@@ -73,7 +102,8 @@ bool loginStatus(int fd, int argc, char *argv[]){
     else if (argc == 5
         && strcmp(argv[1] , "-u") == 0
         && strcmp(argv[3] , "-p") == 0
-    ) { // user
+    ) 
+    { 
         sprintf(buf, "LOGIN %s %s", argv[2], argv[4]);
         write(fd, buf, SIZE_BUFFER);
         puts(buf);
@@ -81,7 +111,8 @@ bool loginStatus(int fd, int argc, char *argv[]){
         strcpy(username, argv[2]);
         strcpy(type, "user");
     } 
-    else {
+    else 
+    {
         puts("Error::Invalid argument");
         return false;
     }
@@ -90,21 +121,21 @@ bool loginStatus(int fd, int argc, char *argv[]){
     return strcmp(buf, "Login success\n") == 0;
 }
  
+char* uppercase(char *str)
+{
+    int i=0, len=strlen(str);
+	char* ret = malloc((len+1) * sizeof(char));
+    
+    while(str[i])
+    {
+        ret[i] = toupper(str[i]); i++;
+    }
+    
+    return ret;
+}
 
-
-bool isValid(char *message){
-
-    /*
-    * Cek:
-    * 1. Jumlah kata
-    * 2. Format perintah
-    * 3. Tidak ada kata yang NULL
-    * 4. Diakhiri dengan titik koma (;)
-    * 5. Atur authorisasi (akses) dari akun user dan root
-    * 
-    * Jika return bernilai false, message tidak dikirim ke server
-    */
-
+bool isValid(char *message)
+{
     if (strcmp(message, "quit") == 0){
         puts("Good bye :3");
         exit(EXIT_SUCCESS);
@@ -116,7 +147,17 @@ bool isValid(char *message){
     return true;
 }
  
+char *trimStr(char *str)
+{
+//     if(str == NULL) return NULL;
 
+//     //size_t len = strlen(str);
+
+//     while(isspace(str[len - 1])) --len;
+//     while(*str && isspace(*str)) ++str, --len;
+
+//     return strndup(str, len);
+}
 
 void *handleInput(void *client_sockets){
 
@@ -127,8 +168,6 @@ void *handleInput(void *client_sockets){
         if (wait) continue;
         printf("%s@%s: ", username, type);
         fgets(message, DATA_BUFFER, stdin);
- 
-        // Remove trailing \n
         char *tmp = strtok(message, "\n");
         if (tmp != NULL) {
             strcpy(message, tmp);
@@ -142,9 +181,46 @@ void *handleInput(void *client_sockets){
     }
 }
 
-   
+char *getStrBetween(char *str, char *PATTERN1, char *PATTERN2)
+{
+    if(PATTERN1 == NULL)
+    {
+        char temp[50]; sprintf(temp, "Temp%s", str);
+        return getStrBetween(temp, "Temp", PATTERN2);
+    }
+    else if(PATTERN2 == NULL)
+    {
+        char temp[50]; sprintf(temp, "%sTemp", str);
+        return getStrBetween(temp, PATTERN1, "Temp");
+    }
 
-void *checkOutput(void *client_sockets){
+    char *target = NULL;
+    char *start, *end;
+
+    if(start = strstr(str, PATTERN1))
+    {
+        start += strlen(PATTERN1);
+        if (end = strstr(start, PATTERN2))
+        {
+            target = (char *)malloc(end - start + 1);
+            memcpy(target, start, end - start);
+            target[end - start] = '\0';
+        }
+    }
+
+    if(target == NULL)
+    {
+        return NULL;
+    }
+    else
+    {
+        return target;
+    }
+}
+
+
+void *checkOutput(void *client_sockets)
+{
 
     int fd = *(int *) client_sockets;
     char message[DATA_BUFFER] = {0};
